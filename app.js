81f282286d47a9b4de4ddfb24d3c339d15e3e97f
Azadeh Hakkak <azadeh.hakkak@gmail.com>
2021-02-23 14:51:04 +1100

    setting up the user routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
 const HttpError = require("./models/http-error");
 
 const app = express();
@@ -11,6 +12,8 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use("/api/places", placesRoutes); // only routes that starts with /api/places can get through
+
+app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
   const error = new HttpError("Could not find this route.", 404);
--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -31,19 +31,19 @@ const getPlaceById = (req, res, next) => {
   res.json({ place }); // { place } => { place: place } if the name of the property is equal to the name of the variable
 };
 
-const getPlaceByUserId = (req, res, next) => {
+const getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((p) => {
+  const places = DUMMY_PLACES.filter((p) => {
     return p.creator === userId;
   });
 
-  if (!place) {
+  if (!places || places.length === 0) {
     // standard code for not finding anything for a given input(404)
     return next(
-      new HttpError("Could not find a place for the provided user id.", 404)
+      new HttpError("Could not find places for the provided user id.", 404)
     );
   }
-  res.json({ place });
+  res.json({ places });
 };
 
 const createPlace = (req, res, next) => {
@@ -85,7 +85,7 @@ const deletePlace = (req, res, next) => {
 
 // exporting multiple function instead of module.export = ... which allow us to only export one thing.
 exports.getPlaceById = getPlaceById;
-exports.getPlaceByUserId = getPlaceByUserId;
+exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
 exports.updatePlace = updatePlace;
 exports.deletePlace = deletePlace;
