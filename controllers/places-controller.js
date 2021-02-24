69d54d1f59f11a93aa4b0b30f6bdfae8d2d30d2b
Azadeh Hakkak <azadeh.hakkak@gmail.com>
2021-02-24 15:02:34 +1100

    converting address to coordinates

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,15 +1,14 @@
-// we can place our middleware functions here and in routes just point to these controllers
-
 const { v4: uuid } = require("uuid");
 const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
+const getCoordsForAddress = require("../util/location");
 
 let DUMMY_PLACES = [
   {
     id: "p1",
     title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world",
+    description: "One of the most famous sky scrapers in the world!",
     location: {
       lat: 40.7484474,
       lng: -73.9871516,
@@ -20,42 +19,56 @@ let DUMMY_PLACES = [
 ];
 
 const getPlaceById = (req, res, next) => {
-  const placeId = req.params.pid; // params property hold an object where our dynamic segment will exist as keys and the value will be the concrete value that the user who sends the request enters as part of URL. { pid: 'p1' }
+  const placeId = req.params.pid; // { pid: 'p1' }
+
   const place = DUMMY_PLACES.find((p) => {
     return p.id === placeId;
   });
+
   if (!place) {
-    return next(
-      new HttpError("Could not find a place for the provided id.", 404)
-    );
+    throw new HttpError("Could not find a place for the provided id.", 404);
   }
-  // send back some data in json format. it can take anything and convert it into json
-  res.json({ place }); // { place } => { place: place } if the name of the property is equal to the name of the variable
+
+  res.json({ place }); // => { place } => { place: place }
 };
+
+// function getPlaceById() { ... }
+// const getPlaceById = function() { ... }
 
 const getPlacesByUserId = (req, res, next) => {
   const userId = req.params.uid;
+
   const places = DUMMY_PLACES.filter((p) => {
     return p.creator === userId;
   });
 
   if (!places || places.length === 0) {
-    // standard code for not finding anything for a given input(404)
     return next(
       new HttpError("Could not find places for the provided user id.", 404)
     );
   }
+
   res.json({ places });
 };
 
-const createPlace = (req, res, next) => {
-  const errors = validationResult(req); // it will look into this object to see if there are any validation errors
-
+const createPlace = async (req, res, next) => {
+  const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new HttpError("Invalid inputs passed, please check your data.", 422);
+    return next(
+      new HttpError("Invalid inputs passed, please check your data.", 422)
+    );
   }
-  // in post requests we expect to get data from the body of the request we have to parse the body in app.js first then we have access to req.body
-  const { title, description, coordinates, address, creator } = req.body;
+
+  const { title, description, address, creator } = req.body;
+
+  let coordinates;
+  try {
+    coordinates = await getCoordsForAddress(address);
+  } catch (error) {
+    return next(error);
+  }
+
+  // const title = req.body.title;
   const createdPlace = {
     id: uuid(),
     title,
@@ -64,26 +77,26 @@ const createPlace = (req, res, next) => {
     address,
     creator,
   };
-  DUMMY_PLACES.push(createdPlace);
+
+  DUMMY_PLACES.push(createdPlace); //unshift(createdPlace)
+
   res.status(201).json({ place: createdPlace });
 };
 
 const updatePlace = (req, res, next) => {
   const errors = validationResult(req);
-
   if (!errors.isEmpty()) {
     throw new HttpError("Invalid inputs passed, please check your data.", 422);
   }
-  // for patch requests we have req.body
-  // we also need the id of the place which is encoded in URL.
-  // so some part of data is in req.body and some part is in URL
 
   const { title, description } = req.body;
   const placeId = req.params.pid;
+
   const updatedPlace = { ...DUMMY_PLACES.find((p) => p.id === placeId) };
   const placeIndex = DUMMY_PLACES.findIndex((p) => p.id === placeId);
   updatedPlace.title = title;
   updatedPlace.description = description;
+
   DUMMY_PLACES[placeIndex] = updatedPlace;
 
   res.status(200).json({ place: updatedPlace });
@@ -92,13 +105,12 @@ const updatePlace = (req, res, next) => {
 const deletePlace = (req, res, next) => {
   const placeId = req.params.pid;
   if (!DUMMY_PLACES.find((p) => p.id === placeId)) {
-    throw new HttpError("Could not find a place for that id", 404);
+    throw new HttpError("Could not find a place for that id.", 404);
   }
   DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
   res.status(200).json({ message: "Deleted place." });
 };
 
-// exporting multiple function instead of module.export = ... which allow us to only export one thing.
 exports.getPlaceById = getPlaceById;
 exports.getPlacesByUserId = getPlacesByUserId;
 exports.createPlace = createPlace;
